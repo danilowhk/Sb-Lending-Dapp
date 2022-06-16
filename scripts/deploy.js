53b71485e5f434d66acbcf53500a7dc1daeec9ce
danilowhk <danilowhk@gmail.com>
2022-06-16 09:20:22 -0300

    deploy script working $ npx hardhat run scripts/deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,20 +6,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const SbLending = await hre.ethers.getContractFactory("sbLending");
+  const sbLending = await SbLending.deploy();
 
-  await greeter.deployed();
+  await sbLending.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("sbLending deployed to:", sbLending.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
