bc647e51e8a961e15cef0abe91dbc4385d4f3f10
Mbappe21 <104240370+Mbappe21@users.noreply.github.com>
2022-06-17 14:45:44 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
+require("@nomiclabs/hardhat-etherscan");
+require('@openzeppelin/hardhat-upgrades');
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.4",
-  hardhat: {
-    forking: {
-      url: "https://eth-mainnet.alchemyapi.io/v2/EhxZmPeqBdUUx7xW2R_Ra6gJhg3hQzmR",
-      blockNumber: 110000
+  networks: {
+    kovan: {
+      url: process.env.KOVAN_URL,
+      accounts: [process.env.KOVAN_PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY
   }
 };
