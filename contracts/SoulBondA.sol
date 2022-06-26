d769e96d5d8bae4bab1cac61c990dadcbbda9a46
Mbappe21 <104240370+Mbappe21@users.noreply.github.com>
2022-06-26 11:14:24 -0500

    Update SoulBondA.sol

--- contracts/SoulBondA.sol
+++ contracts/SoulBondA.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-contract SouldBondA is ERC721 {
+contract SoulBondA is ERC721 {
     address owner;
     uint public counter;
     using Counters for Counters.Counter;
